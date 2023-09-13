0b22eb5232ec8884e258ffe25e362bbcf5bc138e
20230255KIM <20230255@kumoh.ac.kr>
2023-09-13 15:21:33 +0900

    123

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,5 @@
 def print_hello():
     print("Hello world!!")
 
-def print_KIT():
-    print("Kumoh National Institute of Technology")
-
 if __name__ == '__main__':
-    print_KIT()
     print_hello()
